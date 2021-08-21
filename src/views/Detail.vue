3fa37d8e5e1d01bf61371335dca205a84f1e0a2e
zarlinaingtun <zarlinaingtun1999@gmail.com>
2021-08-20 22:54:49 -0700

    post Detail Page and getPost composable function

---
+++ src/views/Detail.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>Detail {{id}}</h1>
+  <h2>{{post.title}}</h2>
+  <p>{{post.body}}</p>
+</template>
+
+<script>
+import getPost from '../composables/getPost'
+export default {
+    
+props:['id'],//id from dynamic route
+setup(props){
+   let {post,error,load}=getPost(props.id);
+   load();
+   return {post,error};
+}
+}
+</script>
+
+<style>
+
+</style>
