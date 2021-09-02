9a2343d60f37c2d6415b8d8551a78e55e483e3f9
zarlinaingtun <zarlinaingtun1999@gmail.com>
2021-09-01 18:10:01 -0700

    get oursingle post doc from firebase

--- src/composables/getPost.js
+++ src/composables/getPost.js
@@ -1,24 +1,31 @@
 import { ref } from "vue"
-
+import {db} from '../firebase/config';
 let getPost=(id)=>{
-let post=ref(null);
-let error=ref('');
-let load=async ()=>{
-    try{
-        await new Promise((resolve,reject)=>{
-            setTimeout(resolve,2000);
-        })
-        let response=await fetch('http://localhost:3000/posts/'+id);
-        if(response.status===404){
-            throw new Error('URL not found');
+    let post=ref(null);
+    let error=ref('');
+    let load=async ()=>{
+        try{
+            //JSON SERVER
+            // await new Promise((resolve,reject)=>{
+            //     setTimeout(resolve,2000);
+            // })
+            // let response=await fetch('http://localhost:3000/posts/'+id);
+            // if(response.status===404){
+            //     throw new Error('URL not found');
+            // }
+            // let data=await response.json();
+            // post.value=data;
+
+            // Firebase
+            let doc=await db.collection('posts').doc(id).get();
+            console.log(doc.data());
+            console.log(doc.id);
+            post.value={id:doc.id,...doc.data()};
         }
-        let data=await response.json();
-        post.value=data;
+        catch(err){
+            error.value=err.message;
+        }
     }
-    catch(err){
-        error.value=err.message;
-    }
-}
-return {post,error,load};
+    return {post,error,load};
 }
 export default getPost;
