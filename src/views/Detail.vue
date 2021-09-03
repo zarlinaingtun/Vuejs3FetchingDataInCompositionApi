27db9710b7d903656be6cda1cbb2ef0ca463f8b9
zarlinaingtun <zarlinaingtun1999@gmail.com>
2021-09-02 20:50:17 -0700

    delete post from firebase by doc id

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -4,6 +4,7 @@
     <h1>Detail {{id}}</h1>
   <h2>{{post.title}}</h2>
   <p>{{post.body}}</p>
+  <button class="del" @click="deletePost">Delete</button>
   </div>
   <div v-else>
     <Spinner></Spinner>
@@ -15,20 +16,32 @@
 import Spinner from '../components/Spinner'
 import getPost from '../composables/getPost'
 import {useRoute} from 'vue-router'
+import {useRouter} from 'vue-router'
+import {db} from '../firebase/config'
+
 export default {
   components: { Spinner },
     
 props:['id'],//id from dynamic route
 setup(props){
-      let router=useRoute();
-      console.log(router);//object
-      console.log(router.params.id);//1,2,3
+      let route=useRoute();
+      let router=useRouter();
+      console.log(route);//object
+      console.log(route.params.id);//1,2,3
      // this.$route.params.id;
 
-   let {post,error,load}=getPost(router.params.id);
+   let {post,error,load}=getPost(route.params.id);
   //  let {post,error,load}=getPost(props.id);
    load();
-   return {post,error};
+
+   let deletePost=async()=>{
+      let id=props.id;
+      await db.collection('posts').doc(id).delete();
+      router.push('/');
+
+      
+   }
+   return {post,error,deletePost};
 }
 }
 </script>
@@ -52,5 +65,16 @@ setup(props){
     border-radius: 50%;
     padding: 10px;
     margin-right: 5px;
+}
+.post .del{
+  display: block;
+  padding:10px;
+  font-size: 20px;
+  background-color:rgb(255, 255, 45);
+  border-radius: 5px;
+  margin:20px auto;
+
+  cursor:pointer;
+
 }
 </style>
